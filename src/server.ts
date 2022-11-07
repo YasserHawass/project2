19828daaa4ae5c0a60a33e9c051cdbcb6f3d6341
Yasser Hawass <yasser.hawass@gmail.com>
2022-11-08 01:23:14 +0200

    fixed bugs

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ import { url } from 'inspector';
   //! END @TODO1
 
   app.get("/filteredimage", async (req: express.Request, res: express.Response) => {
-    let { image_url } = req.query;
+    let { image_url } = req.query.image_url.toString();
     if ( !image_url ) {
       return res.status(400).send("image_url is required");
     }
